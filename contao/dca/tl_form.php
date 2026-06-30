66b805e6d9a1907d6fa655cb042ad1d4512b0dc5
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-30 10:46:27 +0330

    add tl_form.php

---
+++ contao/dca/tl_form.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_form']['fields']['targetTable']['options'][] = 'tl_requests';
+$GLOBALS['TL_DCA']['tl_form']['fields']['targetTable']['options'][] = 'form_orders';
+$GLOBALS['TL_DCA']['tl_form']['fields']['targetTable']['options'][] = 'form_registration';
+$GLOBALS['TL_DCA']['tl_form']['fields']['targetTable']['options'][] = 'form_webinar';

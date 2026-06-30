de8beb0be20eed961ca5a056d65eec6b317f2471
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-30 10:46:13 +0330

    add config.php

---
+++ contao/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of Contao Vatan Bundle.
+ *
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+$GLOBALS['BE_MOD']['vatan']['requests']= array(
+		'tables' => array('tl_requests')
+	);

a0e3ac89f413784614b45427067121c6852b5f93
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-01 20:09:45 +0330

    format by ecs

--- src/ContaoVatanBundle.php
+++ src/ContaoVatanBundle.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Contao Whatsapp Button Bundle.
+ * This file is part of Contao Vatan Bundle.
  *
- * (c) Hamid Peywasti 2025 <hamid@respinar.com>
+ * (c) Hamid Peywasti 2026 <hamid@respinar.com>
  *
  * @license MIT
  */
@@ -18,11 +18,7 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ContaoVatanBundle extends AbstractBundle
 {
-    public function loadExtension(
-        array $config, 
-        ContainerConfigurator $containerConfigurator, 
-        ContainerBuilder $containerBuilder,
-    ): void
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder,): void
     {
         $containerConfigurator->import('../config/services.yaml');
     }

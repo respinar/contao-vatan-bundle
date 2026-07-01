1d52dfb9b4f3e75af58f922a1164e9ead8a75419
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-07-01 20:10:33 +0330

    add copyright

--- src/EventListener/BlogBreadcrumbListener.php
+++ src/EventListener/BlogBreadcrumbListener.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Contao Vatan Bundle.
+ *
+ * (c) Hamid Peywasti 2026 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
 namespace Respinar\ContaoVatanBundle\EventListener;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
